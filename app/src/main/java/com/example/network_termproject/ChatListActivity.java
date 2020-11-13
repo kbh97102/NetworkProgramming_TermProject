3ea37eae3b701a5c7a69a6c1ba26ac7a0124a7af
Arakene <kbh97102@naver.com>
2020-11-13 20:53:34 +0900

    Update ChatListActivity for apply recyclerView

--- app/src/main/java/com/example/network_termproject/ChatListActivity.java
+++ app/src/main/java/com/example/network_termproject/ChatListActivity.java
@@ -4,26 +4,33 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.network_termproject.databinding.ActivityMainBinding;
+import com.example.network_termproject.databinding.ChatListLayoutBinding;
+import com.example.network_termproject.recycler.ListAdapter;
 
 import java.util.ArrayList;
 
 
 public class ChatListActivity extends AppCompatActivity {
 
-    private ActivityMainBinding binding;
+    private ChatListLayoutBinding binding;
     private ArrayList<ChatRoom> chatRooms;
     private ArrayList<Client> clients;
+    private ListAdapter listAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ChatListLayoutBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         init();
-
+        listAdapter = new ListAdapter(chatRooms);
+        binding.chatListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.chatListRecyclerView.setAdapter(listAdapter);
     }
 
     private void init(){
